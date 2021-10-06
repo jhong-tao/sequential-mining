3bdb5e002dc96098db3316bba180c6b145b3d1ee
jhong.tao <jhong.tao@qq.com>
2021-10-06 15:27:42 +0800

    'test'

---
+++ src/prifixSpan.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+==================================================
+@Project -> File   ：sequential-mining -> prifixSpan.py
+@IDE    ：PyCharm
+@Author ：jhong.tao
+@Date   ：2021/6/7 10:34
+@Desc   ：
+==================================================
+"""
+import time
+
+from pyspark.sql import SparkSession
+from pyspark.ml.fpm import PrefixSpan
+from pyspark.sql import Row
+
+if __name__ == '__main__':
+    spark = SparkSession \
+        .builder \
+        .appName("PrefixSpan") \
+        .master("local[*]") \
+        .getOrCreate()
+    sc = spark.sparkContext
+    df = sc.parallelize([Row(sequence=[[1, 2], [3]]),
+                         Row(sequence=[[1], [3, 2], [1, 2]]),
+                         Row(sequence=[[1, 2], [5]]),
+                         Row(sequence=[[6]])]).toDF()
+    ticks = time.time()
+    prefixSpan = PrefixSpan()
+    prefixSpan.getMaxLocalProjDBSize()
+    prefixSpan.getSequenceCol()
+    prefixSpan.setMinSupport(0.5)
+    prefixSpan.setMaxPatternLength(5)
+    prefixSpan.findFrequentSequentialPatterns(df).sort("sequence").show(truncate=False)
+    print(time.time()-ticks)
